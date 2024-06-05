74ac9bc646a391bff97e90309bcfc928d58d3fdc
Mariya Makunina <mariyafrant@mail.ru>
2024-06-05 17:43:45 +0300

    first commit

--- src/test/java/IngredientTypeTest.java
+++ src/test/java/IngredientTypeTest.java
@@ -1,2 +1,27 @@
-package PACKAGE_NAME;public class IngredientTypeTest {
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import praktikum.IngredientType;
+
+@RunWith(Parameterized.class)
+public class IngredientTypeTest {
+    private String ingredientType;
+
+    public IngredientTypeTest(String ingredientType) {
+        this.ingredientType = ingredientType;
+    }
+
+    @Parameterized.Parameters()
+    public static Object[][] getTestData() {
+        return new Object[][]{
+                {"SAUCE"},
+                {"FILLING"}
+        };
+    }
+
+    @Test
+    public void checkValueIngredientTypeTest() {
+        Assert.assertNotNull("Не соответствует ожидаемому результату!", IngredientType.valueOf(ingredientType));
+    }
 }
